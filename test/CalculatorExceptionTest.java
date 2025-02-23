666cd7012fe689c649959dd4b21adc8fe31a36d0
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2025-02-23 11:05:15 -0700

    Testing out the workflow

--- test/CalculatorExceptionTest.java
+++ test/CalculatorExceptionTest.java
@@ -1,17 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class CalculatorExceptionTest {
 
 	private static Calculator calculator;
-	
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		// Test out the workflow
 		calculator = new Calculator();
 	}
 
@@ -29,10 +30,8 @@ class CalculatorExceptionTest {
 
 	@Test
 	void testBZero() {
-		assertThrows(ArithmeticException.class, 
-				() -> calculator.action(5,0)
-		);
-		
+		assertThrows(ArithmeticException.class, () -> calculator.action(5, 0));
+
 	}
 
 }
